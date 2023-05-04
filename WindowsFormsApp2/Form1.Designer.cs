94e3d50c82b4fb9be25e8319f2516b08b1c6d035
rumeysakaycan <rmysnrkycn@gmail.com>
2023-05-04 15:14:50 +0300

    top-oyunu

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnCubuk = new System.Windows.Forms.Panel();
+            this.rdTop = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnCubuk
+            // 
+            this.pnCubuk.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pnCubuk.Location = new System.Drawing.Point(252, 367);
+            this.pnCubuk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnCubuk.Name = "pnCubuk";
+            this.pnCubuk.Size = new System.Drawing.Size(299, 31);
+            this.pnCubuk.TabIndex = 0;
+            this.pnCubuk.Paint += new System.Windows.Forms.PaintEventHandler(this.pnCubuk_Paint);
+            // 
+            // rdTop
+            // 
+            this.rdTop.AutoSize = true;
+            this.rdTop.Location = new System.Drawing.Point(393, 239);
+            this.rdTop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdTop.Name = "rdTop";
+            this.rdTop.Size = new System.Drawing.Size(17, 16);
+            this.rdTop.TabIndex = 1;
+            this.rdTop.TabStop = true;
+            this.rdTop.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Coral;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(127, 46);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(40, 34);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.Location = new System.Drawing.Point(164, 87);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(40, 34);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Location = new System.Drawing.Point(203, 129);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(40, 34);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel4.Location = new System.Drawing.Point(580, 129);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(40, 34);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel5.Location = new System.Drawing.Point(541, 87);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(40, 34);
+            this.panel5.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Coral;
+            this.panel6.Location = new System.Drawing.Point(504, 46);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(40, 34);
+            this.panel6.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.AutoSize = true;
+            this.lblPuan.BackColor = System.Drawing.SystemColors.ControlText;
+            this.lblPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPuan.ForeColor = System.Drawing.Color.Cornsilk;
+            this.lblPuan.Location = new System.Drawing.Point(0, 1);
+            this.lblPuan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(29, 31);
+            this.lblPuan.TabIndex = 7;
+            this.lblPuan.Text = "0";
+            this.lblPuan.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 412);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rdTop);
+            this.Controls.Add(this.pnCubuk);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnCubuk;
+        private System.Windows.Forms.RadioButton rdTop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPuan;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        bool saga = false;
+        bool yukari = true;
+        private int puan =  0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdTop.Checked = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            if (sender == pnCubuk)
+            {
+                x = x + pnCubuk.Left;
+            }
+            int left = x - pnCubuk.Width / 2;
+
+            if (left < 0)
+                left = 0;
+            else if (left > ClientSize.Width - pnCubuk.Width)
+                left = ClientSize.Width - pnCubuk.Width;
+            pnCubuk.Left = left;
+        }
+        bool Carptimi(Panel pn1)
+        {
+
+            if (!pn1.Visible) return false;
+                    if (pn1.Bounds.Contains(rdTop.Right,rdTop.Top+rdTop.Height / 2))
+            {
+                saga = true;
+                rdTop.Left += 5;
+                if (yukari)
+                    rdTop.Top -= 5;
+                else rdTop.Top += 5;
+                pn1.Visible = false;
+                return true;
+
+            }
+                    else if (pn1.Bounds.Contains(rdTop.Left+rdTop.Width/2,rdTop.Bottom))
+            {
+                yukari = true;
+                rdTop.Top -= 5;
+                if (saga)
+                    rdTop.Left += 5;
+                else rdTop.Left -= 5;
+                pn1.Visible = false;
+       
+            }
+            return false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            int x = rdTop.Left;
+            int y = rdTop.Top;
+            if (saga)
+                x += 5;
+            else
+                x -= 5;
+            if (yukari)
+                y -= 5;
+            else
+                y += 5;
+            if (Carptimi(panel1) ||
+                Carptimi(panel2) ||
+                Carptimi(panel3) ||
+                Carptimi(panel4) ||
+                Carptimi(panel5) ||
+                Carptimi(panel6))
+            {
+                puan += 10;
+                lblPuan.Text = puan.ToString();
+                return;
+            }
+            if(x+rdTop.Width>ClientSize.Width) //Sağa Çarptın
+            {
+                x = ClientSize.Width - rdTop.Width;
+                saga = false;
+            }
+             if (y<0)// yukarı çarptın
+            {
+                y = 0;
+                yukari = false;
+            }
+             if(x<0) //Sola carptın
+            {
+                x = 0;
+                saga = true;
+            }
+             if(y+rdTop.Height>ClientSize.Height) //Aşağı Çarptın
+            {
+                y = ClientSize.Height - rdTop.Height;
+                yukari = true;
+                timer1.Enabled = false;
+                //Oyun bitti yazdır
+
+            }
+             if(y+rdTop.Height>pnCubuk.Top && x+rdTop.Width >=pnCubuk.Left && x<=pnCubuk.Right)//
+            {
+                y = pnCubuk.Top - rdTop.Height;
+                yukari = true;
+            }
+            rdTop.Left = x;
+            rdTop.Top = y;
+                    
+                    }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                timer1.Enabled = true;
+                puan = 0;
+                lblPuan.Text = "0";
+                panel1.Visible = true;
+                panel2.Visible = true;
+                panel3.Visible = true;
+                panel4.Visible = true;
+                panel5.Visible = true;
+                panel6.Visible = true;
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Timer1_Tick(this, null);
+        }
+
+        private void pnCubuk_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
